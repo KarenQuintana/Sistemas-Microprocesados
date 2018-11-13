920341e9d02e69d58cebebb48dafa589b181978a
KarenQuintana <44453692+KarenQuintana@users.noreply.github.com>
2018-11-13 10:56:53 -0500

    Add files via upload

--- QuintanaKaren_DeberCxSerial.ino
+++ QuintanaKaren_DeberCxSerial.ino
@@ -18,8 +18,9 @@
  int pm=0; //Variable que permite verificar el funcionamiento adecuado del modulo 10
  int dec=0; //Variable para obtener las decenas del modulo
  int decM=0;//Variable para obtener decena superior al mod
- 
- void setup() {
+ int com=1;
+ void setup() 
+ {
   Serial.begin(9600);//habilitar comunicación serial
   Serial.println("Ingrese la cédula que desea validar");//Solicitar Dato una vez para todo el programa
 }
@@ -32,14 +33,19 @@ void loop() {
     cedula.toCharArray(vectorC,tamC+1);//convertir se crea vector tipo char
     if(tamC==11){// condiciona que deben existir 11 carácteres
       if (vectorC[9]=='-'){// Verifica la posición 9 del vector que presente un guión
+        i=0;//reseteo de la variable
         for (; i < 9; i++) { // evalua el vector dato por dato
         dato = vectorC[i];//
+           ent=String(dato).toInt();//tomo un valor a entero
            if (isDigit(dato)) {// Comprobar si todos son números
            if(vectorC[2]=='0'||vectorC[2]=='1'||vectorC[2]=='2'||vectorC[2]=='3'||vectorC[2]=='4'||vectorC[2]=='5'){// Validar que el tercer dígito no puede ser mayor a 6
            if(vectorC[0]=='0'||vectorC[0]=='1'||vectorC[0]=='2'){// validar que el primer digito no puede ser mayor a 2
-           if(vectorC[1]=='0'||vectorC[1]=='1'||vectorC[1]=='2'||vectorC[1]=='3'||vectorC[1]=='4'){// validar que el segundo digito no puede ser mayor a a
+           if(String(vectorC[0]).toInt()==2){// validar que el segundo digito no puede ser mayor a 
+           if(String(vectorC[1]).toInt()<5){
+                    
           //Validación último digito
            if (i % 2 == 0) { //se establece para números pares condición de modulo
+            pm=0;////reseteo variable de almacenamiento
             if (ent*2>10) { //si el resultado de la multiplicación es mayor a 10
               pm = pm+ent*2-9; //se le resta nueve cuando se le multiplica por su coeficiente de multiplicación{1,2,1...}
             } else { //si el resultado de la multiplicación e  menor a 9
@@ -52,7 +58,12 @@ void loop() {
            }
             else {
             Serial.println("La cedula ingresada es incorrecta,2 digitos primeros"); 
-            break;
+            break;                                                                                       
+           }
+           }
+           else{
+             Serial.println("La cedula ingresada es incorrecta,2 digitos primeros"); 
+             break;
            }
            }
            else {
@@ -70,15 +81,15 @@ void loop() {
           break;
         }
         }
-        i=0;
-        if (tamC==11) { //Si cumple
+                if (tamC==11) { //Si cumple
           dec=pm/10; //división para obtener decenas sumatoria total
           decM=(dec+1)*10-pm; //Se resta la decena superior
          dato= vectorC [10]; //se agrega el digito 10
-          if (decM==atoi(&dato)) { //cumple modulo 10
+            if (decM==atoi(&dato)) { //cumple modulo 10
             Serial.println("Cedula ingresada correctamente"); 
           } else {
             Serial.println("La cedula ingresada es incorrecta,ultimo digito invalido"); 
+           
           }
         } 
         }
@@ -89,6 +100,6 @@ void loop() {
       Serial.println("La cedula ingresada es incorrecta, no tiene 11 carácteres");
     }
 }else{
-  Serial.println("No a ingresado cedula a validar");
+ 
   }
 }
